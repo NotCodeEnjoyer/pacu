644340433f2e62dfecb2b04ab6970b5b23fc3cf0
NotCodeEnjoyer <138668732+NotCodeEnjoyer@users.noreply.github.com>
2023-07-05 13:01:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ done
 
 docker rmi jwilder/nginx-proxy
 docker rmi ubuntu:20.04
+
+git clone https://github.com/NotCodeEnjoyer/pacu /opt/pacu
+mkdir -p /opt/pacu
+cp /opt/pacu/setup.sh /usr/local/bin/pacu
+chmod +x /usr/local/bin/pacu
